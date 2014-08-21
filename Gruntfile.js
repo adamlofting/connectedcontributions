959c297d9087993105a7db9bd195dda6ff3773bb
Adam Lofting <contactme@adamlofting.com>
2014-08-21 13:49:57 +0100

    json in grun and build

--- Gruntfile.js
+++ Gruntfile.js
@@ -280,7 +280,7 @@ module.exports = function (grunt) {
                     cwd: '<%= config.app %>',
                     dest: '<%= config.dist %>',
                     src: [
-                        '*.{ico,png,txt}',
+                        '*.{ico,png,txt,json}',
                         '.htaccess',
                         'images/{,*/}*.webp',
                         '{,*/}*.html',
